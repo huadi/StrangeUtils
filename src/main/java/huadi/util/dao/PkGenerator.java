1666a5406520730c27f100fd7ee32ef525c000aa
HUA Di <huadi223@outlook.com>
2015-10-25 00:01:03 +0800

    Update PkGenerator.java

--- src/main/java/huadi/util/dao/PkGenerator.java
+++ src/main/java/huadi/util/dao/PkGenerator.java
@@ -31,7 +31,7 @@ public class PkGenerator {
           `id` int(11) NOT NULL AUTO_INCREMENT,
           `k` varchar(16) NOT NULL,
           `v` bigint(20) NOT NULL,
-          `step` int(10) unsigned NOT NULL,
+          `step` int(10) unsigned NOT NULL DEFAULT 1,
           `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
           PRIMARY KEY (`id`),
           UNIQUE KEY `IDX_k_UNIQUE` (`k`)
